5463930e85b798877db44f9b22310df500f801d2
bclbrendan <bclbrendan@gmail.com>
2023-02-05 20:08:42 +0000

    added function to convert K to celsius, started to access information from ajax response, added current date

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let APIkey = "3f4a86bb916f63f4d55bdeae5fec0a42"
 var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`
 //var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=50&lon=0&appid=3f4a86bb916f63f4d55bdeae5fec0a42" testing url format
 findLatLong(location)
+let today = moment();
+console.log(today);
 
 $.ajax(
 {url: queryURL,
@@ -15,7 +17,16 @@ method: "GET"
 })
 .then(function(response) { 
     console.log(response)
-    $("#today").text("test")
+    console.log(response.list[0].main.temp)
+    var tempK = response.list[0].main.temp
+    KelvintoCelsius(tempK) //converting temp via function
+    console.log(celsius) // converted temp from K to celsius
+
+    $("#today").text("today's weather" + today.format("D MMM YYYY") + " " + celsius)
+    
+
+    $("#forecast").text("5 day's weather")
+
 
 }
 
@@ -25,3 +36,11 @@ getLocation();
 function findLatLong(city){
 
 }
+/*
+function todaysDate(){
+    let today = moment();
+    return today
+}
+*/
+function KelvintoCelsius(temp){
+    return celsius = temp - 273.15 }
