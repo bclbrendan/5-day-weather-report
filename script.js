18b1ad67a368cf8b3e82b8612133f83bba730428
bclbrendan <bclbrendan@gmail.com>
2023-02-05 19:17:10 +0000

    added reset.css, confirmed api key and url working, started function for translating city to lat/long

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+//default lat long will be london area
+
+let lat = 51.507351
+let lon = -0.127758
 
 function getLocation(location){
-let lat = ""
-let lon = ""
 let APIkey = "3f4a86bb916f63f4d55bdeae5fec0a42"
-var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`
+var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`
+//var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=50&lon=0&appid=3f4a86bb916f63f4d55bdeae5fec0a42" testing url format
+findLatLong(location)
+
 $.ajax(
 {url: queryURL,
 method: "GET"
+})
+.then(function(response) { 
+    console.log(response)
+    $("#today").text("test")
+
 }
-)
+
+)}
+getLocation();
+
+function findLatLong(city){
+
 }
